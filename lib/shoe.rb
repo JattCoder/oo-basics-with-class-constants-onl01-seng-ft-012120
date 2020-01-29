80fb67a84a23cacc15fc139c168ca737a0e88c7c
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-29 03:52:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,11 +3,23 @@ class Shoe
   attr_reader :brand
 
   BRANDS = []
+<<<<<<< HEAD
 
   def initialize(brand)
     @brand = brand
     BRANDS << brand
     BRANDS.uniq!
+=======
+  fav_brands = ["Uggs", "Rainbow"]
+  def initialize(brand)
+    @brand = brand
+    BRANDS << brand
+    count = 0
+    while count < BRANDS.length
+      BRANDS.delete_at(count) if BRANDS[count].exclude?(fav_brands)
+      count += 1
+    end
+>>>>>>> 5bf7df1981f54b2fe2c20e04439442a92c7023ed
   end
 
   def cobble
